11f1da2747d78311452e82fd368878fb46ce4fc7
amkeegan <andrewk.47@hotmail.com>
2019-08-29 09:58:45 +1000

    Updated with new resampling method
    Script should now record at native 48KHz and then down sample to 8KHz using scipy.signal.resample. Due to the nature of audio resampling the resultant audio isn't great - plenty of room for experimentation/research.

--- audio/real-time-encoding-demo/wave_test.py
+++ audio/real-time-encoding-demo/wave_test.py
@@ -2,15 +2,18 @@ import pyaudio      # Connects with Portaudio for audio device streaming
 import wave         # Used to save audio to .wav files
 import collections  # Used for Double ended Queue (deque) structure
 #import msvcrt       # Used for non-blocking keyboard event
+from scipy import signal
+import numpy
+#import librosa
 
 from pydub import AudioSegment
 
 # Define Audio characteristics
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
-RATE = 8000
+RATE = 48000
 CHUNK = 1024
-RECORD_SECONDS = 10
+RECORD_SECONDS = 2
 WAVE_OUTPUT_FILENAME = "file.wav"
 FLAC_OUTPUT_FILENAME = "file.flac"
 AAC_OUTPUT_FILENAME = "file.aac"
@@ -22,7 +25,7 @@ stream = audio.open(format=FORMAT, channels=CHANNELS,rate=RATE, input=True,frame
                 
 print ('recording...')
 
-data = stream.read(8 * CHUNK * RECORD_SECONDS)
+data = stream.read(int(RATE/CHUNK) * CHUNK * RECORD_SECONDS)
 
 
 print ('finished recording')
@@ -32,17 +35,21 @@ stream.stop_stream()
 stream.close()
 audio.terminate()
  
-waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-waveFile.setnchannels(CHANNELS)
-waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-waveFile.setframerate(RATE)
-waveFile.writeframes(data)
-waveFile.close()
-
 # Pydub addition - will export raw audio - data - into specified formats.
-newrecording = AudioSegment(data, sample_width=2, frame_rate=8000, channels=1)
-newrecording.export(WAVE_OUTPUT_FILENAME, format="wav")     # Raw audio at 161KB per 10 seconds
-newrecording.export(FLAC_OUTPUT_FILENAME, format = "flac")  # ~50% reduction in file size; 91KB per 10 seconds
+recording = AudioSegment(data, sample_width=2, frame_rate=RATE, channels=1)
+recording.export("raw.wave", format="wav")     # Raw audio at 161KB per 10 seconds
+recording.export("raw.flac", format = "flac")  # ~50% reduction in file size; 91KB per 10 seconds
+recording_samples = recording.get_array_of_samples()
+recording_resample = signal.resample(recording_samples, 8000)
+
+resamples = b''
+for i in range(0, len(recording_resample)):
+    resamples += (int(recording_resample[i].astype(numpy.int16)/32).to_bytes(4, byteorder='big', signed=True))
+print(resamples)
+
+newrecording = AudioSegment(resamples, sample_width=2, frame_rate=8000, channels=1)
+newrecording.export("resample.wav", format="wav")     # Raw audio at 161KB per 10 seconds
+newrecording.export("resample.flac", format = "flac")  # ~50% reduction in file size; 91KB per 10 seconds
 #newrecording.export(AAC_OUTPUT_FILENAME, format = "aac")   # Currently produces an error - unresolved github issue
 
 # Desktop conversion results in 53KB; ~70% reduction
