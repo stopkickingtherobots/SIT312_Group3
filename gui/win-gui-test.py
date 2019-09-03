d745bc036a4c00a2517dd06ac613b4260f055ae7
amkeegan <andrewk.47@hotmail.com>
2019-09-03 10:47:51 +1000

    Added gui test/demo

---
+++ gui/win-gui-test.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+# Reference: https://gist.github.com/sergeyfarin/c689fd0171f95865055fad857579bc94
+
+import sys
+import os
+# import site
+# site.addsitedir('/usr/local/lib/python2.7/site-packages')
+from PyQt5 import QtCore, QtWidgets, QtWebEngineWidgets
+
+app = QtWidgets.QApplication(sys.argv)
+view = QtWebEngineWidgets.QWebEngineView()
+
+view.load(QtCore.QUrl().fromLocalFile(os.path.split(os.path.abspath(__file__))[0]+r'\html\plot_points_to_map.html'))
+
+view.show()
+sys.exit(app.exec_())
