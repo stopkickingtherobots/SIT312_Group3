73bac4c3ef34d8fe748174741d0f8974541514b6
Ian-RG <ian.goodwin@hotmail.com.au>
2019-09-04 15:45:39 +1000

    Add playback information

--- ui/ui.py
+++ ui/ui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 import tkinter.font
 from time import localtime, strftime, sleep
 from PIL import ImageTk, Image
@@ -44,8 +45,7 @@ def display_voice_message_warning_countdown(countdown):
     if countdown == 1:
         window.after(1000, lambda: display_record_voice_message_countdown(10))
     else:
-        window.after(
-            1000, lambda: display_voice_message_warning_countdown(countdown-1))
+        window.after(1000, lambda: display_voice_message_warning_countdown(countdown-1))
         # Start thread for voice recording here
         # window.after(1000, ***THREAD START***)
 
@@ -56,8 +56,7 @@ def display_record_voice_message_countdown(timeframe):
         enable_controls()
         return
     activity_label.config(text="Record your message\n{0}".format(timeframe))
-    window.after(
-        1000, lambda: display_record_voice_message_countdown(timeframe-1))
+    window.after(1000, lambda: display_record_voice_message_countdown(timeframe-1))
 
 
 def record_voice_message():
@@ -91,21 +90,47 @@ def play_pause_audio():
         play_pause_button.configure(image=play_img)
         play_pause_button.image = play_img
 
+        play_pause_display_icon.configure(image=pause_icon)
+        play_pause_display_icon.image = pause_icon
+
     else:
         play_pause_button.configure(image=pause_img)
         play_pause_button.image = pause_img
+
+        play_pause_display_icon.configure(image=play_icon)
+        play_pause_display_icon.image = play_icon
 
     is_playing_audio = not is_playing_audio
 
 
 def rewind_track():
     # Restart current audio here
+    playback_prog_bar["value"] = 0
     activity_label.config(text="Rewind")
 
 
 def skip_track():
     # Jump to next track in queue here
+    playback_prog_bar["value"] = 0
+    global current_track
+    current_track += 1
+    text = test_track_info[current_track]["title"]
+
+    track_label.config(text=text)
     activity_label.config(text="Skip")
+
+def update_playback_progress():
+    if is_playing_audio:
+        playback_prog_bar["value"] += 1
+
+        total_mins = test_track_info[current_track]["length"] // 60
+        total_secs = test_track_info[current_track]["length"] % 60
+        current_mins = playback_prog_bar["value"] // 60
+        current_secs = playback_prog_bar["value"] % 60
+
+        text = "{0}:{1} / {2}:{3}".format(current_mins, current_secs, total_mins, total_secs)
+        playback_prog_label.config(text=text)
+    window.after(1000, update_playback_progress)
 
 
 def monitor_input():
@@ -129,6 +154,9 @@ pijuice = PiJuice(1, 0x14)
 
 is_playing_audio = False
 is_input_enabled = True
+#Test data for track playing
+test_track_info = [{"title": "Test Track 1", "length": 215}, {"title": "Test Track 2", "length": 351}, {"title": "Test Track 3", "length": 111}]
+current_track = 0
 
 window = Tk()
 window.config(cursor='none')
@@ -141,7 +169,7 @@ status_font = tkinter.font.Font(
     family="Times New Roman", size=11, weight="bold")
 
 
-# Frame for status bar
+# Status frame
 status_frame = Frame(window, borderwidth=1, relief="solid", bg="white")
 status_frame.pack(side=TOP)
 
@@ -170,55 +198,68 @@ gps_label = Label(status_frame, image=gps_img)
 gps_label.pack(side=RIGHT)
 
 # padding frame
-padding_frame = Frame(window, height=20, bg="white")
-padding_frame.pack()
+padding_frame_1 = Frame(window, height=20, bg="white")
+padding_frame_1.pack()
 
 # placeholder for testing inputs
 activity_frame = Frame(window, borderwidth=1, relief="solid")
 activity_frame.pack(side=TOP)
-activity_label = Label(activity_frame, text="Pause",
-                       font=("Times New Roman", 24, "bold"))
+activity_label = Label(activity_frame, text="Pause", font=("Times New Roman", 24, "bold"))
 activity_label.pack(side=TOP)
 
+# padding frame
+padding_frame_2 = Frame(window, height=20, bg="white")
+padding_frame_2.pack()
+
+# Playback information
+playback_info_frame = Frame(window)
+playback_info_frame.pack()
+
+pause_icon = ImageTk.PhotoImage(Image.open("./resources/pause_icon.png"))
+play_icon = ImageTk.PhotoImage(Image.open("./resources/play_icon.png"))
+
+play_pause_display_icon = Label(playback_info_frame, image=pause_icon, padx=10)
+play_pause_display_icon.pack(side=LEFT)
+
+playback_prog_bar = ttk.Progressbar(playback_info_frame, orient="horizontal", length=150, mode="determinate")
+playback_prog_bar["value"] = 0
+playback_prog_bar["maximum"] = test_track_info[0]["length"]
+playback_prog_bar.pack(side=LEFT)
+
+mins = test_track_info[0]["length"]//60
+secs = test_track_info[0]["length"] % 60
+playback_prog_label = Label(playback_info_frame, text="0:00 / {0}:{1}".format(mins, secs))
+playback_prog_label.pack(side=LEFT)
+
+bottom_info_frame = Frame(window)
+bottom_info_frame.pack()
+track_label = Label(bottom_info_frame, text=test_track_info[0]["title"])
+track_label.pack(side=BOTTOM)
+
+# padding frame
+padding_frame_3 = Frame(window, height=20, bg="white")
+padding_frame_3.pack()
+
+# Playback controls
 play_img = ImageTk.PhotoImage(Image.open("./resources/play_button.png"))
 pause_img = ImageTk.PhotoImage(Image.open("./resources/pause_button.png"))
 rewind_img = ImageTk.PhotoImage(Image.open("./resources/back_button.png"))
 skip_img = ImageTk.PhotoImage(Image.open("./resources/forward_button.png"))
+playback_control_frame = Frame(window)
+playback_control_frame.pack(side=BOTTOM)
 
-playback_frame = Frame(window)
-playback_frame.pack(side=BOTTOM)
-
-rewind_button = Button(playback_frame, image=rewind_img,
-                       command=rewind_track)
+rewind_button = Button(playback_control_frame, image=rewind_img, command=rewind_track)
 rewind_button.pack(side=LEFT)
 
-play_pause_button = Button(
-    playback_frame, image=play_img, command=play_pause_audio)
+play_pause_button = Button(playback_control_frame, image=play_img, command=play_pause_audio)
 play_pause_button.pack(side=LEFT)
 
-skip_button = Button(playback_frame, image=skip_img, command=skip_track)
+skip_button = Button(playback_control_frame, image=skip_img, command=skip_track)
 skip_button.pack(side=RIGHT)
-
-
-# # playback controls frame
-# playback_frame=Frame(window)
-# playback_frame.pack(side=BOTTOM)
-
-# rewind_img=ImageTk.PhotoImage(Image.open("./resources/back_button.png"))
-# rewind_button=Button(playback_frame, image=rewind_img, command=rewind_track)
-# rewind_button.pack(side=LEFT)
-
-# play_pause_img=ImageTk.PhotoImage(Image.open("./resources/play_button.png"))
-# play_pause_button=Button(
-#     playback_frame, image=play_pause_img, command=play_pause_audio)
-# play_pause_button.pack(side=LEFT)
-
-# skip_img=ImageTk.PhotoImage(Image.open("./resources/forward_button.png"))
-# skip_button=Button(playback_frame, image=skip_img, command=skip_track)
-# skip_button.pack(side=RIGHT)
 
 
 window.protocol("WM_DELETE_WINDOW", close)
 window.after(5000, refresh_status_bar)
 window.after(100, monitor_input)
+window.after(1000, update_playback_progress)
 window.mainloop()
