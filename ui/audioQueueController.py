04f95dabe5d8028eb44bb8b72946b19487e17e4c
Ian-RG <30923025+Ian-RG@users.noreply.github.com>
2019-09-16 13:28:21 +1000

    Create audio queue controller

---
+++ ui/audioQueueController.py
@@ -0,0 +1,37 @@
+from multiprocessing import Process, Queue
+from time import strftime, localtime
+import json
+
+def add_audio(audio):
+    new_data = {"filename": audio, "title": f"Base Message {strftime('%H:%M', localtime())}"}
+    with open("./audio/tracklist.json", "r+") as file:
+        data = list(json.load(file))
+        data.append(new_data)
+        print(data)
+        file.seek(0)
+        json.dump(data, file)
+        file.truncate()        
+
+
+def get_audio():
+    result = ""
+    with open("./audio/audio_out/outlist.json", "r+") as file:
+        data = list(json.load(file))
+        for i in range(len(data)):
+            if not data[i]["is_sent"]:
+                result = data[i]["filename"]
+                data[i]["is_sent"] = True
+                file.seek(0)
+                json.dump(data, file)
+                file.truncate()
+                return result
+    return False
+
+def main(audio_queue_out, audio_queue_in):
+    audio = audio_queue_in.get()
+    if audio != None:
+        add_audio(audio)
+
+    out_audio = get_audio()
+    if out_audio:
+        audio_queue_out.put(out_audio)
